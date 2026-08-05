37a42fffca93c33d2735765f26c8e7baa56e8178
Vishavjit Singh <vishav4848@gmail.com>
2026-08-05 10:53:03 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (46.41%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.py
+++ 58-length-of-last-word/length-of-last-word.py
@@ -3,7 +3,7 @@ class Solution(object):
         i = len(s) - 1
         length = 0
 
-        while s[i] == " ":
+        while  i >= 0 and s[i] == " ":
             i -=1
         while  i >= 0 and s[i] != " ":
                 
