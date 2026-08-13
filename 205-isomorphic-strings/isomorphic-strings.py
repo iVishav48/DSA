096bb871d8f223af10b0ed1325209c54badd1fef
Vishavjit Singh <vishav4848@gmail.com>
2026-08-13 22:14:04 +0530

    Time: 19 ms (9.64%) | Memory: 12.8 MB (96.41%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.py
@@ -0,0 +1,13 @@
+
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        map1 = []
+        map2 = []
+        for i in s:
+            map1.append(s.index(i))
+        for i in t:
+            map2.append(t.index(i))
+        if map1 == map2:
+            return True
+        return False
+        
